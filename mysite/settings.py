8058cc45dc8417a484409c8cf945d74979106beb
muhammadd03 <muhammad.03kh@gmail.com>
2026-06-04 06:27:03 +0500

    fix whitenoise static files storage

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
