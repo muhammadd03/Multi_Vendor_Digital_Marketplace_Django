aefd84561a46b465015e14638d0f5a8b8a5feb56
muhammadd03 <muhammad.03kh@gmail.com>
2026-06-04 05:27:36 +0500

    fix CSRF trusted origins for Render

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key-change-in-production')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://' + host for host in ALLOWED_HOSTS if host != '*']
 
 INSTALLED_APPS = [
     'myapp',
